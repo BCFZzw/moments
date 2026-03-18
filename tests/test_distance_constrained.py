4414d684a74cd47958cf869f740441dde8b10484
Alouette Zhang <alouettezhang@macbookpro.home>
2026-03-18 18:34:20 -0400

    fix:fixed bugs related to testing functions

--- tests/test_distance_constrained.py
+++ tests/test_distance_constrained.py
@@ -3,8 +3,8 @@ import unittest
 import moments.LD
 
 class Test_simple_constrianed_genotype_count(unittest.TestCase):
-    L = 3
-    n = 5
+    L = 5
+    n = 6
     np.random.seed(5553)
     G = np.random.randint(3, size=L * n).reshape(L, n)
     # working as the previous unmodified version
@@ -34,7 +34,7 @@ class Test_simple_constrianed_genotype_count(unittest.TestCase):
     def test_not_constrained(self):
         pos_array = np.array([1, 2, 3, 4, 5], dtype = np.int32)
         threshold = 0
-        D2_pw, Dz_pw, pi2_pw, D_pw = moments.LD.Parsing.compute_pairwise_stats(self.G, pos_array, genotypes = True, distance_constrained = threshold)
+        D2_pw, Dz_pw, pi2_pw, D_pw = moments.LD.Parsing.compute_pairwise_stats(self.G,  genotypes = True, pos_array = pos_array, distance_constrained = threshold)
         self.assertTrue(np.all(D2_pw == self.D2_pw))
         self.assertTrue(np.all(Dz_pw == self.Dz_pw))
         self.assertTrue(np.all(D_pw == self.D_pw))
@@ -43,7 +43,7 @@ class Test_simple_constrianed_genotype_count(unittest.TestCase):
     def test_all_filtered(self):
         pos_array = np.array([1, 2, 3, 4, 5], dtype = np.int32)
         threshold = 10
-        D2_pw, Dz_pw, pi2_pw, D_pw = moments.LD.Parsing.compute_pairwise_stats(self.G, pos_array, genotypes = True, distance_constrained = threshold)
+        D2_pw, Dz_pw, pi2_pw, D_pw = moments.LD.Parsing.compute_pairwise_stats(self.G, genotypes = True, pos_array = pos_array, distance_constrained = threshold)
         self.assertTrue(np.all(D2_pw == None))
         self.assertTrue(np.all(Dz_pw == None))
         self.assertTrue(np.all(D_pw == None))
